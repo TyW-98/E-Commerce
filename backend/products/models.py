954e4e0f8927724a523cdd9e26b77919fd879349
wey1998 <yitwey1998@hotmail.com>
2023-08-08 17:50:09 +0100

    Handle static image files

--- backend/products/models.py
+++ backend/products/models.py
@@ -8,6 +8,7 @@ class Product(models.Model):
     posterID = models.ForeignKey(User, on_delete = models.SET_NULL, null=True, verbose_name="Seller ID")
     name = models.CharField(max_length=255, null=False, blank=False)
     brand = models.CharField(max_length=255, null=False, blank=False)
+    image = models.ImageField(null=True, blank=True)
     category = models.CharField(max_length=255, null=False, blank=False)
     description = models.TextField(null = False, blank=False)
     rating = models.DecimalField(max_digits=3, decimal_places=2, default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
@@ -18,7 +19,7 @@ class Product(models.Model):
     createdAt = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
-        return self.name (self.brand)
+        return f"{self.name} - {self.brand}"
     
 class Review(models.Model):
     userID = models.ForeignKey(User, on_delete = models.CASCADE, null=False, verbose_name="User ID")

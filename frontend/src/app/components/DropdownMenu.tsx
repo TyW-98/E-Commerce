f82e4cb1888065d0def7ee1d283405ce6db41525
wey1998 <yitwey1998@hotmail.com>
2023-08-14 13:06:35 +0100

    Add dropdown-menu links

--- frontend/src/app/components/DropdownMenu.tsx
+++ frontend/src/app/components/DropdownMenu.tsx
@@ -13,6 +13,31 @@ export default function DropdownMenu() {
     setIsOpen(false);
   };
 
+  const menuItems = [
+    {
+      title: "Featured",
+      links: [
+        "Best Sellers",
+        "New Arrivals",
+        "Top Rated",
+        "Deals of the Day",
+        "Clearance",
+      ],
+    },
+    {
+      title: "Categories",
+      links: ["Laptops", "Smartphones", "Tablets", "Accessories", "Gaming"],
+    },
+    {
+      title: "Brands",
+      links: ["Apple", "Samsung", "HP", "Microsoft", "Sony"],
+    },
+    {
+      title: "Product Types",
+      links: ["Monitors", "Keyboards", "Headphones", "Cameras", "Printers"],
+    },
+  ];
+
   return (
     <div
       className="flex h-12 w-[65px] items-center "
@@ -23,13 +48,28 @@ export default function DropdownMenu() {
       <div
         className={`${
           isOpen ? "inline-block" : "hidden"
-        } absolute left-0 bg-orange-300 w-full top-[70px] border-t border-black`}
+        } absolute left-0 bg-white w-full top-[70px] border-t border-black z-10`}
       >
-        <ul>
-          <li>1</li>
-          <li>1</li>
-          <li>1</li>
-          <li>1</li>
+        <ul className="grid grid-cols-4 text-center">
+          {menuItems.map((cat) => {
+            return (
+              <li key={cat.title}>
+                <ul className="flex flex-col gap-3 my-3">
+                  <li className="text-xl font-bold underline">{cat.title}</li>
+                  {cat.links.map((item, index) => {
+                    return (
+                      <li
+                        key={index}
+                        className="cursor-pointer hover:underline active:underline active:to-blue-600"
+                      >
+                        <a>{item}</a>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>

6ce028fd6147adb5cb305a928d1b587d30611c2f
wey1998 <yitwey1998@hotmail.com>
2023-08-22 17:06:28 +0100

    Setup docker configuration for backend database

--- backend/products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import GetProductsView, GetProductView, GetRoutesView
-
-urlpatterns = [
-    path("", GetRoutesView.as_view(), name="Home Route"),
-    path("products/", GetProductsView.as_view(), name="Products Route"),
-    path("products/<int:pk>/", GetProductView.as_view(), name="Product Route")
-]
